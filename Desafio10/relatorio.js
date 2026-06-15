37a69a0027cd1e7bc5ad47278647272d239d8fcb
theLucasb <lucasrodrigodes@gmail.com>
2026-06-15 14:06:31 -0300

    DESAFIO 10 - SISTEMA DE FUNCIONÁRIOS

---
+++ Desafio10/relatorio.js
@@ -0,0 +1,17 @@
+/*
+DESAFIO 10 - SISTEMA DE FUNCIONÁRIOS
+
+O setor de Recursos Humanos precisa gerar um relatório simples.
+
+Crie um array de objetos contendo:
+- nome do funcionário
+- salário
+
+Desenvolva funções para:
+
+1. Listar todos os funcionários.
+2. Calcular a folha salarial da empresa.
+3. Identificar o funcionário com o maior salário.
+
+Exiba os resultados de forma organizada no console.
+*/
