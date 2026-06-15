2138673a7d90fad44e6ed4c2ed4016950f324f31
theLucasb <lucasrodrigodes@gmail.com>
2026-06-15 14:05:41 -0300

    DESAFIO 08 - MINI SISTEMA DE LOGIN

---
+++ Desafio08/login.js
@@ -0,0 +1,22 @@
+/*
+DESAFIO 08 - MINI SISTEMA DE LOGIN
+
+Crie um sistema simples de autenticação.
+
+Cadastre:
+- usuário
+- senha
+
+Crie uma função chamada login() que receba:
+- usuário digitado
+- senha digitada
+
+O sistema deve verificar as informações
+e exibir:
+
+"Acesso permitido"
+
+ou
+
+"Acesso negado"
+*/
