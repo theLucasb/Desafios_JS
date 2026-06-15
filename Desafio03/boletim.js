f39ea290bd3fac78f46eb0f0304fe4651afb1652
theLucasb <lucasrodrigodes@gmail.com>
2026-06-15 14:03:02 -0300

    DESAFIO 03 - SISTEMA DE APROVAÇÃO

---
+++ Desafio03/boletim.js
@@ -0,0 +1,14 @@
+/*
+DESAFIO 03 - SISTEMA DE APROVAÇÃO
+
+Crie uma função chamada verificarAluno() que receba:
+- nome
+- nota
+
+Regras:
+- Nota maior ou igual a 7 → Aprovado
+- Nota maior ou igual a 5 e menor que 7 → Recuperação
+- Nota menor que 5 → Reprovado
+
+Exiba uma mensagem informando a situação do aluno.
+*/
