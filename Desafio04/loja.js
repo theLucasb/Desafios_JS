e7d760e6a7044e17b2dc8395fd229aec45b8c74e
theLucasb <lucasrodrigodes@gmail.com>
2026-06-15 14:03:23 -0300

    DESAFIO 04 - LOJA TECH

---
+++ Desafio04/loja.js
@@ -0,0 +1,14 @@
+/*
+DESAFIO 04 - LOJA TECH
+
+Uma loja de informática possui vários produtos cadastrados.
+
+Crie um array de objetos contendo:
+- nome
+- preco
+
+Utilize uma estrutura de repetição para mostrar apenas
+os produtos que custam mais de R$ 100,00.
+
+Exiba o nome dos produtos encontrados.
+*/
