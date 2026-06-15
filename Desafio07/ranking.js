c9caadc7427bac23c531e97f8832a488251ff8cc
theLucasb <lucasrodrigodes@gmail.com>
2026-06-15 14:04:45 -0300

    DESAFIO 07 - RANKING DE JOGADORES

---
+++ Desafio07/ranking.js
@@ -0,0 +1,16 @@
+/*
+DESAFIO 07 - RANKING DE JOGADORES
+
+Uma competição precisa exibir a classificação dos participantes.
+
+Crie um array contendo o nome de 4 jogadores.
+
+Utilize uma estrutura de repetição para mostrar:
+
+1º Jogador
+2º Jogador
+3º Jogador
+4º Jogador
+
+A posição deve ser gerada automaticamente pelo sistema.
+*/
