744b1275da65eaeede8f7e84de3e6f708b1ca4fa
theLucasb <lucasrodrigodes@gmail.com>
2026-06-15 14:05:59 -0300

    DESAFIO 09 - CONTROLE DE ESTOQUE

---
+++ Desafio09/estoque.js
@@ -0,0 +1,17 @@
+/*
+DESAFIO 09 - CONTROLE DE ESTOQUE
+
+Uma empresa deseja monitorar seu estoque de produtos.
+
+Crie um array de objetos contendo:
+- nome do produto
+- quantidade em estoque
+
+O sistema deve:
+
+1. Listar todos os produtos cadastrados.
+2. Mostrar apenas os produtos com estoque inferior a 10 unidades.
+3. Calcular a quantidade total de itens armazenados.
+
+Exiba todas as informações no console.
+*/
