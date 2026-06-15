4073ae86d0afc5d32b5bb03582f90a65b737e973
theLucasb <lucasrodrigodes@gmail.com>
2026-06-15 14:03:59 -0300

    DESAFIO 05 - CONTADOR INTELIGENTE

---
+++ Desafio05/contador.js
@@ -0,0 +1,12 @@
+/*
+DESAFIO 05 - CONTADOR INTELIGENTE
+
+Crie uma função chamada contador() que receba:
+- valor inicial
+- valor final
+
+A função deve exibir todos os números entre os valores informados.
+
+Após concluir, crie uma segunda versão capaz de realizar
+a contagem regressiva.
+*/
