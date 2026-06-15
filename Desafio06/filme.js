7a9b00b4fb7d6e551b3a9ad236ecafbe2bc10126
theLucasb <lucasrodrigodes@gmail.com>
2026-06-15 14:04:15 -0300

    DESAFIO 06 - FILME FAVORITO

---
+++ Desafio06/filme.js
@@ -0,0 +1,14 @@
+/*
+DESAFIO 06 - FILME FAVORITO
+
+Crie um objeto chamado filme contendo:
+- titulo
+- genero
+- ano
+
+Crie uma função responsável por receber esse objeto
+e exibir todas as informações formatadas no console.
+
+O objetivo é praticar a manipulação de objetos
+e passagem de parâmetros para funções.
+*/
