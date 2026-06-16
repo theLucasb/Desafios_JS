8e3547cbfd8e16b2c2c1e70fb49cde23ec5eea23
theLucasb <lucasrodrigodes@gmail.com>
2026-06-15 21:04:12 -0300

    Desafio 03 Concluído

--- Desafio03/boletim.js
+++ Desafio03/boletim.js
@@ -6,9 +6,25 @@ Crie uma função chamada verificarAluno() que receba:
 - nota
 
 Regras:
-- Nota maior ou igual a 7 → Aprovado
-- Nota maior ou igual a 5 e menor que 7 → Recuperação
-- Nota menor que 5 → Reprovado
+- Nota maior ou igual a 7 = Aprovado
+- Nota maior ou igual a 5 e menor que 7 = Recuperação
+- Nota menor que 5 = Reprovado
 
 Exiba uma mensagem informando a situação do aluno.
 */
+function verificarAluno(nome, nota) {
+
+    if(nota >= 7) {
+        console.log(nome + " está aprovado(a)");
+    }
+    else if(nota >= 5) {
+        console.log(nome + " está em recuperação");
+    }
+    else {
+        console.log(nome + " está reprovado(a)");
+    }
+}
+
+verificarAluno("Maria", 7);
+verificarAluno("João", 5.5);
+verificarAluno("Carlos", 3);
