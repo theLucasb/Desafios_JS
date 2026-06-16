fee0c10af1701753513644f16a864e6d0aa8d6ec
theLucasb <lucasrodrigodes@gmail.com>
2026-06-15 21:03:48 -0300

    Desafio 02 Concluído

--- Desafio02/media.js
+++ Desafio02/media.js
@@ -9,22 +9,16 @@
     Mostrar no console/output o seguinte:
     Média da turma: 10.0
 */
-/* let notas = [10.0, 8.5, 7.5, 6.0, 9.0];
 
-let somar = 0;
-
-for(resultado = 0; resultado <= notas.length; resultado++) {
-    somar =+ notas[resultado];    
-}
-*/
+//             0  1  2   3   4  = 5
 const notas = [8, 7, 9, 10, 6]; 
 
 let soma = 0;
 
 for(let i = 0; i < notas.length; i++) {
-    soma += notas[i];
+    soma = soma + notas[i];
 }
 
 let media = soma / notas.length;
 
-console.log("Média da turma:", media);
+console.log("Média da turma: ", media);
