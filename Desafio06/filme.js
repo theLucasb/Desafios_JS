64dfbe7b7cde96507d0e02729dc3de6e4643687d
theLucasb <lucasrodrigodes@gmail.com>
2026-06-22 21:46:39 -0300

    Desafio Filme concluído

--- Desafio06/filme.js
+++ Desafio06/filme.js
@@ -7,8 +7,25 @@ Crie um objeto chamado filme contendo:
 - ano
 
 Crie uma função responsável por receber esse objeto
-e exibir todas as informações formatadas no console.
+e exibir todas as informações formatadas no console
+utilizando template string.
 
 O objetivo é praticar a manipulação de objetos
 e passagem de parâmetros para funções.
 */
+
+let filme = {
+    titulo: "Shrek",
+    genero: "Comédia",
+    ano: 2005
+}
+
+function exibirFilme() {
+    console.log(`
+        O título do filme que irei assistir 
+        no cinema nesse fim de semana é: ${filme.titulo}. 
+        O gênero do filme é de: ${filme.genero}, 
+        do ano de ${filme.ano}. `);
+}
+
+exibirFilme();
