1528cd1f9a30d7dd884db7fda782ef84b0d0c2e9
theLucasb <lucasrodrigodes@gmail.com>
2026-06-22 21:45:31 -0300

    Desafio loja concluído

--- Desafio04/loja.js
+++ Desafio04/loja.js
@@ -12,3 +12,16 @@ os produtos que custam mais de R$ 100,00.
 
 Exiba o nome dos produtos encontrados.
 */
+
+let produtos = [
+    {nome: "Teclado", preco: 159.90},
+    {nome: "Mouse Gamer", preco: 249.90},
+    {nome: "Fone de Ouvido", preco: 89.90}
+];
+
+
+for(let i = 0; i < produtos.length; i++) {
+    if(produtos[i].preco > 100){
+        console.log(produtos[i].nome);
+    }
+}
