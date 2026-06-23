40150c9db02868d4765cef7561771d49a8c8d1aa
theLucasb <lucasrodrigodes@gmail.com>
2026-06-22 21:46:21 -0300

    Desafio contador concluído

--- Desafio05/contador.js
+++ Desafio05/contador.js
@@ -10,3 +10,20 @@ A função deve exibir todos os números entre os valores informados.
 Após concluir, crie uma segunda versão capaz de realizar
 a contagem regressiva.
 */
+
+function contador(inicial, final) {
+    console.log("Contador Ascendente");
+    for (let i = inicial; i <= final; i++) {
+        console.log(i);
+    }
+}
+
+function contadorRegressivo(inicial, final) {
+    console.log("Contador Decrescente");
+    for (let i = inicial; i >= final; i--) {
+        console.log(i);
+    }
+}
+
+contador(0, 10);
+contadorRegressivo(10, 0);
